05da42490c568e5283a9367db8a0b42406d427de
Nirav Soni <soninirav910@gmail.com>
2024-01-06 12:45:57 +0530

    Post with stream body

--- Movies.Client/Services/StreamService.cs
+++ Movies.Client/Services/StreamService.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
@@ -23,14 +24,16 @@ namespace Movies.Client.Services
         public async Task Run()
         {
             //await GetPosterWithStream();
-            //await
-            //GetPosterWithStreamAndCompletionMode();
-            await TestGetPosterWithoutStream();
-            await TestGetPosterWithStream();
-            await TestGetPosterWithStreamAndCompletionMode();
+            //await GetPosterWithStreamAndCompletionMode();
+            await PostPosterWithStream();
+
+            //Test methods
+            //await TestGetPosterWithoutStream();
+            //await TestGetPosterWithStream();
+            //await TestGetPosterWithStreamAndCompletionMode();
         }
 
-        #region Implementation
+        #region Get Methods
         /// <summary>
         /// Read Json response as Stream - Better and more memory efficient
         /// </summary>
@@ -122,6 +125,50 @@ namespace Movies.Client.Services
             var posters = JsonConvert.DeserializeObject<Poster>(content);
         }
 
+        #endregion
+
+        #region Post Methods
+        private async Task PostPosterWithStream()
+        {
+            // generate a movie poster of 500KB
+            var random = new Random();
+            var generatedBytes = new byte[524288];
+            random.NextBytes(generatedBytes);
+
+            var posterForCreation = new PosterForCreation(generatedBytes, "A new poster for The Big Lebowski");
+            
+            //This is what we used to convert object to string in CRUD Service
+            // ==> JsonConvert.SerializeObject(posterForCreation);
+
+            //This is going to hold our data when posting it
+            var memoryContentStream = new MemoryStream();
+            
+            //Serialize to Json and Write to memory stream
+            memoryContentStream.SerializeToJsonAndWrite(posterForCreation);
+
+            //Set to position 0 since that's where we want to start streaming it from
+            memoryContentStream.Seek(0, SeekOrigin.Begin);
+
+            using (var request = new HttpRequestMessage(
+               HttpMethod.Post,
+               $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters"))
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                using (var streamContent = new StreamContent(memoryContentStream))
+                {
+                    streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    request.Content = streamContent;
+
+                    var response = await _httpClient.SendAsync(request);
+                    response.EnsureSuccessStatusCode();
+
+                    var createdContent = await response.Content.ReadAsStringAsync();
+                    var createdPoster = JsonConvert.DeserializeObject<Poster>(createdContent);
+
+                    // do something with the newly created poster     
+                }
+            }
+        }
         #endregion
 
         #region Test Methods
