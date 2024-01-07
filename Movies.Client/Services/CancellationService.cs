8f5fb29e56a534dfb75c410a0c92bddcd1a8c193
Nirav Soni <soninirav910@gmail.com>
2024-01-07 10:53:30 +0530

    Better approach to catch exception

--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -16,6 +16,7 @@ namespace Movies.Client.Services
                 AutomaticDecompression = System.Net.DecompressionMethods.GZip
             });
 
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         public CancellationService()
         {
             // set up HttpClient instance
@@ -26,10 +27,11 @@ namespace Movies.Client.Services
 
         public async Task Run()
         {
-            await GetTrailerAndCancel();
+            _cancellationTokenSource.CancelAfter(1000);
+            await GetTrailerAndCancel(_cancellationTokenSource.Token);
         }
 
-        private async Task GetTrailerAndCancel()
+        private async Task GetTrailerAndCancel(CancellationToken cancellationToken)
         {
             var request = new HttpRequestMessage(
                     HttpMethod.Get,
@@ -37,19 +39,27 @@ namespace Movies.Client.Services
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
 
-            //Approach#1 - Creating CancellationTokenSource object and setting making sure Cancellation is forcefully triggered bu calling - Cancel/CancelAfter() methods
-            var cancellationTokenSource = new CancellationTokenSource();
-            //This will cancel after 1 second
-            cancellationTokenSource.CancelAfter(1000);
+            ////Approach#1 - Creating CancellationTokenSource object and setting making sure Cancellation is forcefully triggered bu calling - Cancel/CancelAfter() methods
+            //var cancellationTokenSource = new CancellationTokenSource();
+            ////This will cancel after 1 second
+            //cancellationTokenSource.CancelAfter(1000);
 
-            using (var response = await _httpClient.SendAsync(request,
-                    HttpCompletionOption.ResponseHeadersRead,
-                    cancellationTokenSource.Token))
+            try
             {
-                var stream = await response.Content.ReadAsStreamAsync();
+                using (var response = await _httpClient.SendAsync(request,
+                    HttpCompletionOption.ResponseHeadersRead,
+                    cancellationToken))
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
 
-                response.EnsureSuccessStatusCode();
-                var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+                    response.EnsureSuccessStatusCode();
+                    var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+                }
+            }
+            catch (OperationCanceledException ocException)
+            {
+                Console.WriteLine($"An operation was cancelled with message {ocException.Message}.");
+                // additional cleanup, ...
             }
         }
     }
