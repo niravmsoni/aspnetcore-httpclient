aa4b227cbedda7550ad464355d932b3225a2d8c2
Nirav Soni <soninirav910@gmail.com>
2024-01-07 10:57:55 +0530

    Updates - Understood timeout

--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -21,14 +21,16 @@ namespace Movies.Client.Services
         {
             // set up HttpClient instance
             _httpClient.BaseAddress = new Uri("http://localhost:57863");
-            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.Timeout = new TimeSpan(0, 0, 2);
             _httpClient.DefaultRequestHeaders.Clear();
         }
 
         public async Task Run()
         {
-            _cancellationTokenSource.CancelAfter(1000);
-            await GetTrailerAndCancel(_cancellationTokenSource.Token);
+            //_cancellationTokenSource.CancelAfter(1000);
+            //await GetTrailerAndCancel(_cancellationTokenSource.Token);
+
+            await GetTrailerAndHandleTimeout();
         }
 
         private async Task GetTrailerAndCancel(CancellationToken cancellationToken)
@@ -49,6 +51,31 @@ namespace Movies.Client.Services
                 using (var response = await _httpClient.SendAsync(request,
                     HttpCompletionOption.ResponseHeadersRead,
                     cancellationToken))
+                {
+                    var stream = await response.Content.ReadAsStreamAsync();
+
+                    response.EnsureSuccessStatusCode();
+                    var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+                }
+            }
+            catch (OperationCanceledException ocException)
+            {
+                Console.WriteLine($"An operation was cancelled with message {ocException.Message}.");
+                // additional cleanup, ...
+            }
+        }
+
+        private async Task GetTrailerAndHandleTimeout()
+        {
+            var request = new HttpRequestMessage(
+                  HttpMethod.Get,
+                  $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/trailers/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            try
+            {
+                using (var response = await _httpClient.SendAsync(request,
+                    HttpCompletionOption.ResponseHeadersRead))
                 {
                     var stream = await response.Content.ReadAsStreamAsync();
 
