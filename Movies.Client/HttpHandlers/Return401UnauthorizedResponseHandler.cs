16eb863f10912b45025fca03e42ee1ed3ff8e87f
Nirav Soni <soninirav910@gmail.com>
2024-01-07 15:16:08 +0530

    Added 401 Response delegating handler

---
+++ Movies.Client/HttpHandlers/Return401UnauthorizedResponseHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.Client
+{
+    /// <summary>
+    /// Using this delegating handler in Unit tests.
+    /// This would help us write a unit test that mimicks as if we got Unauthorized response from actual downstream API
+    /// </summary>
+    public class Return401UnauthorizedResponseHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            return Task.FromResult(response);
+        }
+    }
+}
